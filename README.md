7e76aa79421f8d85bb8739bfe1de4f8f5890e85b
Joao Figueira <jpaulogvf@gmail.com>
2014-11-09 00:04:08 +0000

    vagrant LAMP PHP app skeleton

---
+++ README.md
@@ -0,0 +1,16 @@
+Provisioning Vagrant with a LAMP server
+=======================================
+
+Requires VirtualBox and Vagrant installed.
+
+
+You can use this as a starting point to your PHP projects
+
+
+Usage:
+------
+```
+vagrant up
+```
+
+Have fun!
---
+++ provision/bootstrap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Updating ..."
+apt-get update > /dev/null
+
+echo "Installing Apache"
+apt-get install -y apache2 php5 libapache2-mod-php5 > /dev/null
+
+echo "Configuring Apache..."
+cp -f /vagrant/provision/000-default.conf /etc/apache2/sites-available/000-default.conf
+a2enmod rewrite >/dev/null
+cp -f /vagrant/provision/servername.conf /etc/apache2/conf-available/servername.conf
+a2enconf servername > /dev/null
+service apache2 restart > /dev/null
+
+echo "Installing MySQL"
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+apt-get -y install mysql-server > /dev/null
+
+apt-get install -y mysql-client php5-mysql > /dev/null
+
+echo "Installing Composer"
+curl -sS https://getcomposer.org/installer | php > /dev/null
+mv composer.phar /usr/local/bin/composer
+
+echo "Installing Git"
+apt-get install -y git > /dev/null
+
+if [ -f /vagrant/composer.json ]; then
+    echo "composer.json found, let's setup your project"
+    composer --working-dir=/vagrant/ install
+fi
+
+echo "Done Installing stuff. Have a nice day!"
