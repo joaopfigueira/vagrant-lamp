302e89619c4e5c4d3789fec733434d37955d83b2
Joao Figueira <jpaulogvf@gmail.com>
2016-12-01 12:38:38 +0000

    upgraded ubuntu to 16.04

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "ubuntu/xenial64"
   config.vm.network "forwarded_port", guest: 80, host: 8080
   config.vm.network "forwarded_port", guest: 1080, host: 1080
   config.vm.provision :shell, path: "provision/bootstrap.sh"
--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -29,12 +29,12 @@ GIT_USER="Git User"
 GIT_EMAIL=user.email@domain.com
 
 # Node stuff
-USE_NODE=false
-USE_GULP=false
-USE_BOWER=false
-USE_GRUNT=false
+ USE_NODE=false
+ USE_GULP=false
+ USE_BOWER=false
+ USE_GRUNT=false
 
-USE_MAILCATCHER=false
+ USE_MAILCATCHER=false
 
 ###
 # end configuration
@@ -49,7 +49,7 @@ echo '/var/swap.1 none swap sw 0 0' >> /etc/fstab
 echo "Updating ..."
 apt-get -qq update > /dev/null 2>&1
 
-sed -i 's|\\u@\\h:\\w\\$ |\\n\\u@\\h:\\w\\n\\$ |g' /home/vagrant/.bashrc
+sed -i 's|\\u@\\h:\\w\\$ |\\n\\u@\\h:\\w\\n\\$ |g' /home/ubuntu/.bashrc
 
 if $USE_APACHE; then
     echo "Installing Apache"
@@ -63,13 +63,13 @@ fi
 
 if $USE_PHP; then
     echo "Installing PHP"
-    apt-get -qq -y install php5 libapache2-mod-php5 > /dev/null 2>&1
-    apt-get -qq -y install php5-curl > /dev/null 2>&1
-    apt-get -qq -y install php5-gd > /dev/null 2>&1
-    apt-get -qq -y install php5-sqlite > /dev/null 2>&1
-    apt-get -qq -y install php5-mcrypt > /dev/null 2>&1
-    php5enmod mcrypt
-    cp -f /vagrant/provision/php.ini /etc/php5/apache2/php.ini
+    apt-get -qq -y install php libapache2-mod-php > /dev/null 2>&1
+    apt-get -qq -y install php-curl > /dev/null 2>&1
+    apt-get -qq -y install php-gd > /dev/null 2>&1
+    apt-get -qq -y install php-sqlite3 > /dev/null 2>&1
+    apt-get -qq -y install php-mcrypt > /dev/null 2>&1
+    phpenmod mcrypt
+    cp -f /vagrant/provision/php.ini /etc/php/7.0/apache2/php.ini
 fi
 
 if $USE_MYSQL; then
@@ -77,7 +77,7 @@ if $USE_MYSQL; then
     debconf-set-selections <<< "mysql-server mysql-server/root_password password $DB_PASSWD"
     debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DB_PASSWD"
     apt-get -qq -y install mysql-server > /dev/null 2>&1
-    apt-get -qq -y install mysql-client php5-mysql > /dev/null  2>&1
+    apt-get -qq -y install mysql-client php-mysql > /dev/null  2>&1
 
     echo "Creating Database"
     mysql -uroot -p$DB_PASSWD -e "CREATE DATABASE $DB_NAME" >> /dev/null 2>&1
@@ -114,9 +114,13 @@ if $USE_GIT; then
     echo "Installing Git"
     apt-get install -y git > /dev/null 2>&1
     
-    echo "[user]" >> /home/vagrant/.gitconfig
-    echo "    name = $GIT_USER" >> /home/vagrant/.gitconfig
-    echo "    email = $GIT_EMAIL" >> /home/vagrant/.gitconfig
+    echo "[user]" >> /home/ubuntu/.gitconfig
+    echo "    name = $GIT_USER" >> /home/ubuntu/.gitconfig
+    echo "    email = $GIT_EMAIL" >> /home/ubuntu/.gitconfig
+
+    git clone https://github.com/magicmonty/bash-git-prompt.git /home/ubuntu/.bash-git-prompt --depth=1 > /dev/null 2>&1
+    echo "GIT_PROMPT_ONLY_IN_REPO=1" >> /home/ubuntu/.bashrc
+    echo "source ~/.bash-git-prompt/gitprompt.sh" >> /home/ubuntu/.bashrc
 fi
 
 if $USE_PHPUNIT; then
@@ -149,9 +153,8 @@ if $USE_GRUNT && $USE_NODE; then
 fi
 
 if $USE_MAILCATCHER; then
-    apt-get -qq -y install build-essential libsqlite3-dev ruby1.9.1-dev > /dev/null 2>&1
-    gem install mime-types --version "< 3" > /dev/null 2>&1
-    gem install --conservative mailcatcher > /dev/null 2>&1
+    apt-get -qq -y install build-essential libsqlite3-dev ruby2.3-dev > /dev/null 2>&1
+    gem install mailcatcher --no-ri --no-rdoc > /dev/null 2>&1
     echo 'description "Mailcatcher"' > /etc/init/mailcatcher.conf
     echo '' >> /etc/init/mailcatcher.conf
     echo 'start on runlevel [2345]' >> /etc/init/mailcatcher.conf
@@ -160,7 +163,7 @@ if $USE_MAILCATCHER; then
     echo 'respawn' >> /etc/init/mailcatcher.conf
     echo 'exec /usr/bin/env $(which mailcatcher) --foreground --http-ip=0.0.0.0' >> /etc/init/mailcatcher.conf
     if $USE_PHP; then
-        sed -i 's|^;sendmail_path =|sendmail_path = /usr/bin/env /usr/local/bin/catchmail|g' /etc/php5/apache2/php.ini
+        sed -i 's|^;sendmail_path =|sendmail_path = /usr/bin/env /usr/local/bin/catchmail|g' /etc/php/7.0/apache2/php.ini
     fi
     /usr/bin/env $(which mailcatcher) --ip=0.0.0.0 > /dev/null 2>&1
 fi
